bac3cfb102ba4262509dac41fd2ec5793d44a896
weilun <tangweilun0410@gmail.com>
2024-11-09 09:02:51 +0800

    Fix the page don't touch the edge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="container">{children}</div>
+        <div className="">{children}</div>
       </body>
     </html>
   );
