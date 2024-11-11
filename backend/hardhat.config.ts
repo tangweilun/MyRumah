13de77128efa8a94fc51642469df73fa7e2a72c1
Jeffwy <wongyejeff@gmail.com>
2024-11-11 17:55:35 +0800

    initial commit

---
+++ backend/hardhat.config.ts
@@ -0,0 +1,15 @@
+import type { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox-viem';
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.27',
+
+  networks: {
+    localhost: {
+      chainId: 31337,
+      url: 'http://127.0.0.1:8545',
+    },
+  },
+};
+
+export default config;
