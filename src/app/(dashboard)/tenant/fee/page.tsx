29818b3dc1bc4a1b16f816cf51590da7cd0b8634
WyFong <fweiye18@gmail.com>
2024-11-12 22:40:13 +0800

    updated tenant fee management page

---
+++ src/app/(dashboard)/tenant/fee/page.tsx
@@ -0,0 +1,19 @@
+import { Clock } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+
+export default function FeeManagementPage() {
+  return (
+    <div>
+      <div className="bg-green-100 w-screen py-16">
+        <div className="items-start justify-between p-6">
+          <div>
+            <h1 className="text-3xl font-bold text-green-900 mb-4">
+              Fee Management
+            </h1>
+          </div>
+          <div className="border bg-white p-2"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
