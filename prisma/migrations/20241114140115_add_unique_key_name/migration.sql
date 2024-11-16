0dc5b22a05d9229f0832a3174aa6c3469a4bd93a
ckyeow0922 <165008086+ckyeow0922@users.noreply.github.com>
2024-11-16 16:28:06 +0800

    update

--- prisma/migrations/20241114140115_add_unique_key_name/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[email,role]` on the table `UserInfo` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "UserInfo_email_role_key" ON "UserInfo"("email", "role");
