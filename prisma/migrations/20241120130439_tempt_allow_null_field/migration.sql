e645250066fb25044a6f5e64788e47c7983beb20
Jeffwy <wongyejeff@gmail.com>
2024-11-23 15:41:55 +0800

    some update

--- prisma/migrations/20241120130439_tempt_allow_null_field/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "PropertyInfo" ALTER COLUMN "image" DROP NOT NULL,
-ALTER COLUMN "modified_date" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "Proposal" ALTER COLUMN "modified_date" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "RentalFee" ALTER COLUMN "modified_date" DROP NOT NULL;
